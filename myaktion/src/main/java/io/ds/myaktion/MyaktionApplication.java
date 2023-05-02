1d31519ecee4dc671f1c7b1bf6f2d53860bd0a2f
Nico Döbele <nico.doebele@googlemail.com>
2023-05-02 15:41:56 +0200

    initial commit

---
+++ myaktion/src/main/java/io/ds/myaktion/MyaktionApplication.java
@@ -0,0 +1,13 @@
+package io.ds.myaktion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyaktionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyaktionApplication.class, args);
+	}
+
+}
---
+++ myaktion/src/test/java/io/ds/myaktion/MyaktionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ds.myaktion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyaktionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
